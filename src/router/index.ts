e0eb1e5f6d865060348815dfc63311e03ebd20f2
bcastlel <bcastlel@users.noreply.github.com>
2022-04-20 13:37:26 +0300

    add home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
+import ComponentPage from '@/views/ComponentPage.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/component',
+    name: 'ComponentPage',
+    component: ComponentPage,
   },
 ];
 
---
+++ src/views/ComponentPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="component-page">
+    Component Page
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ComponentPage',
+});
+</script>
